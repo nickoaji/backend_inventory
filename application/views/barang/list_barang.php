d66e88c8b69a34640702c0524fa31b15c438b725
nickoaji <nicko.10@students.amikom.ac.id>
2021-06-24 22:05:14 +0700

    Initial commit

---
+++ application/views/barang/list_barang.php
@@ -0,0 +1,12 @@
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-body">
+        <h4>Dibawah Ini Adalah Data Barang</h4>
+        <table id="tabel_barang" class="table">
+
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
